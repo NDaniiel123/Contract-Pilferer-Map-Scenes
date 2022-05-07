4397768abba2882c833ce62521cd3aa1cde84629
NDaniiel123 <103097086+NDaniiel123@users.noreply.github.com>
2022-05-08 02:09:15 +1000

    smooth fov
    ...

--- Assets/Scripts/FOV.cs
+++ Assets/Scripts/FOV.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class FOV : MonoBehaviour
 {
@@ -8,17 +9,40 @@ public class FOV : MonoBehaviour
     {
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        Vector3[] vertices = new Vector3[3];
-        Vector2[] uv = new Vector2[3];
-        int[] triangles = new int[3];
 
-        vertices[0] = Vector3.zero;
-        vertices[1] = new Vector3(5, 0);
-        vertices[2] = new Vector3(0, -5);
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 5f;
 
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
+        Vector3[] vertices = new Vector3[rayCount + 1 +1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+
+        for (int i = 0; i <= rayCount; i++) 
+        {
+            Vector3 vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;           
+        }
 
         mesh.vertices = vertices;
         mesh.uv = uv;
